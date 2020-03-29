ff096856ebe3e24ce97ec3c5a1b40578f1d3a9cc
rlq <rlqjue@gmail.com>
2020-03-29 18:28:29 +0800

    update MainFragment

--- app/src/main/java/com/he/lq/demo/MainFragment.kt
+++ app/src/main/java/com/he/lq/demo/MainFragment.kt
@@ -5,13 +5,12 @@ import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.he.lq.ble.BleFragment
 import com.android.he.lq.sdk.MotionFragment
-import io.flutter.facade.FlutterFragment
+import io.flutter.embedding.android.FlutterFragment
 
 /*
     两个Fragment切换：FlutterFragment + MotionFragment
@@ -51,7 +50,8 @@ class MainFragment : Fragment() {
         override fun getItem(position: Int): Fragment {
             when (position) {
                 0 -> {
-                    return FlutterFragment()
+                    // 删除io.flutter.facade 包下的FlutterFragment
+                    return FlutterFragment.createDefault()
                 }
                 1 -> {
                     return MotionFragment()
